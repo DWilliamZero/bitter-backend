8621abf72a0562dc089eec2f587661d541af7ec7
DWilliamZero <william.zero@gigkloud.com>
2020-04-10 19:02:56 -0400

    limit usernames to 12 chars

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   
   has_secure_password
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { maximum: 12 }
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6 }
