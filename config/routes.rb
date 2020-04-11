84586bdbd8a76ad52739b2f3295a51c4d0280001
DWilliamZero <william.zero@gigkloud.com>
2020-04-11 10:53:55 -0400

    add direct posts end-point

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
       resources :hates
     end
   end
-
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
